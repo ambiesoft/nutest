4d96979c20c86b186b01d77eea12f4e29bd7d215
ambiesoft <ambiesoft.trueff@gmail.com>
2021-01-13 02:15:07 +0900

    start

---
+++ nutest/nutest.cpp
@@ -0,0 +1,18 @@
+// nutest.cpp : main project file.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace nutest;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew Form1());
+	return 0;
+}
